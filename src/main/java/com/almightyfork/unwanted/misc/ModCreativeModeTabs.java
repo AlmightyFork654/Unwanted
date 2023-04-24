59d2c5093f8e9f2a58b57513ebee9b7cc69e659c
DarKing4888 <104357512+DarKing4888@users.noreply.github.com>
2023-04-24 17:15:40 +0200

    creative tab

--- src/main/java/com/almightyfork/unwanted/misc/ModCreativeModeTabs.java
+++ src/main/java/com/almightyfork/unwanted/misc/ModCreativeModeTabs.java
@@ -7,15 +7,17 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Unwanted.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModCreativeModeTabs {
     public static CreativeModeTab ITEMS_TAB;
 
+    @SubscribeEvent
     public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event) {
         ITEMS_TAB = event.registerCreativeModeTab(new ResourceLocation(Unwanted.MODID, "items_tab"),
                 builder -> builder.icon(() -> new ItemStack(ModItems.RUBY.get()))
-                        .title(Component.translatable("itemGroup.ItemsTab")));
+                        .title(Component.translatable("creativemodetab.items_tab")));
     }
 }
